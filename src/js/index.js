6b0240aa88621485f210793c482f0a382e0b623d
Luciano Carvalho <lucianocarv13@gmail.com>
2022-04-22 23:01:42 -0300

    22/04/2022 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+// Exclusive Login Button
 const headerLinks = Array.from(document.getElementsByClassName('h-nav-link'))
 const lastLink = headerLinks.length - 1
 const loginLink = headerLinks[lastLink]
@@ -6,4 +7,32 @@ const arrayOfAttr = headerLinks[lastLink].attributes.getNamedItem('gs-bt-login')
 if ((arrayOfAttr.value = 'gs-bt-login')) {
   loginLink.setAttribute('id', 'id-login')
   loginLink.style.color = '#fff'
+}
+
+// Menu
+let bodyHtml = document.body
+const menuButton = document.getElementById('bt-nav-menu-id')
+menuButton.setAttribute('onclick', 'clickMenuForDisplay()')
+let menuHeader = document.createElement('div')
+menuHeader.setAttribute('class', 'display-menu')
+menuHeader.setAttribute('id', 'display-menu-id')
+menuHeader.style.position = 'absolute'
+menuHeader.style.width = '0px'
+menuHeader.style.height = '100vh'
+menuHeader.style.backgroundColor = 'forestGreen'
+menuHeader.style.top = '0'
+bodyHtml.appendChild(menuHeader)
+let menuDisplay = document.querySelector('#display-menu-id')
+let attrMenu = menuDisplay.ATTRIBUTE_NODE
+
+// Event Menu Display
+const clickMenuForDisplay = () => {
+  if (menuHeader.style.width == '0px') {
+    menuHeader.style.width = '300px'
+    menuHeader.style.transition = 'all .5s ease'
+    console.log('Mostrou')
+  } else {
+    menuHeader.style.width = '0px'
+    console.log('Recolheu')
+  }
 }
