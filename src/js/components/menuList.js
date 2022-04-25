dec859e3cb7b17a340061ba804784af05c890811
Luciano Carvalho <lucianocarv13@gmail.com>
2022-04-24 22:42:20 -0300

    Adição de responsividade e correção no menu

---
+++ src/js/components/menuList.js
@@ -0,0 +1,11 @@
+let setLinksMenu = [
+  'hardware',
+  'periféricos',
+  'games',
+  'cadeiras',
+  'computadores',
+  'celular e smartphones',
+  'tv',
+  'tablets',
+  'outros',
+]
--- src/js/index.js
+++ src/js/index.js
@@ -10,38 +10,42 @@ if ((arrayOfAttr.value = 'gs-bt-login')) {
 }
 
 // Menu
-let bodyHtml = document.body
-let menuRightWidth = '400px'
-let menuRighHiddenWidth = `-${menuRightWidth}`
+let bodyHtml = document.getElementById('absolute-content')
+let menuLeftWidth = '300px'
+let menuLeftHiddenWidth = `-${menuLeftWidth}`
 let menuDefaultTransition = 'all .5s ease'
 let menuButton = document.getElementById('bt-nav-menu-id')
 menuButton.setAttribute('onclick', 'clickMenuForDisplay()')
 let menuHeader = document.createElement('div')
 menuHeader.setAttribute('class', 'display-menu')
 menuHeader.setAttribute('id', 'display-menu-id')
-menuHeader.style.width = menuRightWidth
-menuHeader.style.marginRight = menuRighHiddenWidth
+menuHeader.style.width = menuLeftWidth
+menuHeader.style.marginLeft = menuLeftHiddenWidth
 bodyHtml.appendChild(menuHeader)
 let menuDisplay = document.querySelector('#display-menu-id')
+
+let getHeaderHeight = document.getElementById('h-body-id')
+let clientHeightHeader = getHeaderHeight.clientHeight
+menuDisplay.style.top = `${clientHeightHeader}px`
+menuDisplay.style.height = '100vh'
 
 // Event Menu Display
 const clickMenuForDisplay = () => {
   let menuOpen = document.getElementById('bt-menu-h')
-  if (menuHeader.style.marginRight == menuRighHiddenWidth) {
+  menuButton.style.zIndex = '2'
+  if (menuHeader.style.marginLeft == menuLeftHiddenWidth) {
     if (document.body.clientWidth > 600) {
       menuButton.style.transform = 'rotateX(180deg)'
-      menuHeader.style.marginRight = '0px'
-      menuHeader.style.position = 'fixed'
-      menuOpen.setAttribute('class', 'fa fa-rectangle-xmark')
+      menuHeader.style.marginLeft = '0px'
+      menuOpen.setAttribute('class', 'fa fa-xmark')
       menuButton.setAttribute('onclick', 'clickMenuClose()')
     } else {
-      menuRightWidth = '70%'
-      menuHeader.style.width = menuRightWidth
+      menuLeftWidth = '300px'
+      menuHeader.style.width = menuLeftWidth
       menuHeader.style.display = 'block'
       menuButton.style.transform = 'rotateX(180deg)'
-      menuHeader.style.marginRight = '0px'
-      menuHeader.style.position = 'fixed'
-      menuOpen.setAttribute('class', 'fa fa-rectangle-xmark')
+      menuHeader.style.marginLeft = '0px'
+      menuOpen.setAttribute('class', 'fa fa-xmark')
       menuButton.setAttribute('onclick', 'clickMenuClose()')
     }
   }
@@ -50,7 +54,7 @@ const clickMenuForDisplay = () => {
 const clickMenuClose = () => {
   menuOpen = document.getElementById('bt-menu-h')
   menuButton.style.transform = 'rotateX(-180deg)'
-  menuHeader.style.marginRight = menuRighHiddenWidth
+  menuHeader.style.marginLeft = menuLeftHiddenWidth
   menuOpen.setAttribute('class', 'fa fa-bars')
   menuButton.style.transform = 'rotateX()'
   menuButton.style.transform = 'none'
@@ -61,38 +65,28 @@ const clickMenuClose = () => {
 // Mobile
 
 // Menu Content
-let menuContainerTagOpen = '<nav class="menu-nav-right" id="righ-nav-content">'
+let menuContainerTagOpen = '<nav class="menu-nav-right" id="right-nav-content">'
 let menuContainerTagClose = '</nav>'
 let menuLinkOpen = '<a class="menu-li-link-a"'
 let menuLinkClose = '</a>'
 menuDisplay.innerHTML = menuContainerTagOpen + menuContainerTagClose
 let setMenuConatinerListOpen = '<ul id="ex-list-menu">'
 let setMenuConatinerListClose = '</ul>'
-let getMenuArea = document.getElementById('righ-nav-content')
+let getMenuArea = document.getElementById('right-nav-content')
 getMenuArea.innerHTML = setMenuConatinerListOpen + setMenuConatinerListClose
 let getMenuAreaUl = document.getElementById('ex-list-menu')
 let getMenuUL = getMenuAreaUl.setAttribute('class', 'ex-menu-list-r')
-const setLinksMenu = [
-  'hardware',
-  'periféricos',
-  'games',
-  'cadeiras',
-  'computadores',
-  'celular e smartphones',
-  'tv',
-  'tablets',
-  'outros',
-]
+
 setLinksMenu.forEach((link) => {
   let liLink = document.createElement('li')
-  let iconBeforeLink = '<i class="fa-solid fa-angle-left"></i>'
+  let iconAfterLinkTab = '<i class="fa-solid fa-angle-right"></i>'
   liLink.setAttribute('class', 'li-link-nav-right')
   liLink.innerHTML =
     menuLinkOpen +
     `href="${link.toLowerCase()}-page"` +
     '>' +
-    iconBeforeLink +
     `<p class="text-link-m-right">${link}</p>` +
+    iconAfterLinkTab +
     menuLinkClose
   getMenuAreaUl.insertAdjacentElement('beforeend', liLink)
 })
