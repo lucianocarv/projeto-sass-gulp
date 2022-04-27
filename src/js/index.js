a79de21f8485706c36b3e77ac2a673340e40ab0b
Luciano Carvalho <lucianocarv13@gmail.com>
2022-04-27 13:28:38 -0300

    Mudança de Cores

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
-// Exclusive Login Button
-const headerLinks = Array.from(document.getElementsByClassName('h-nav-link'))
-const lastLink = headerLinks.length - 1
-const loginLink = headerLinks[lastLink]
-loginLink.setAttribute('gs-bt-login', 'gs-bt-login')
-const arrayOfAttr = headerLinks[lastLink].attributes.getNamedItem('gs-bt-login')
-if ((arrayOfAttr.value = 'gs-bt-login')) {
-  loginLink.setAttribute('id', 'id-login')
-  loginLink.style.color = '#fff'
-}
+// // Exclusive Login Button
+// const headerLinks = Array.from(document.getElementsByClassName('h-nav-link'))
+// const lastLink = headerLinks.length - 1
+// const loginLink = headerLinks[lastLink]
+// loginLink.setAttribute('gs-bt-login', 'gs-bt-login')
+// const arrayOfAttr = headerLinks[lastLink].attributes.getNamedItem('gs-bt-login')
+// if ((arrayOfAttr.value = 'gs-bt-login')) {
+//   loginLink.setAttribute('id', 'id-login')
+//   loginLink.style.color = '#fff'
+// }
 
 // Menu
 let bodyHtml = document.getElementById('absolute-content')
